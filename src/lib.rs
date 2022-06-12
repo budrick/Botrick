babfbf5b869bec51394550ca43e4243618357fe2
Matt Cegielka <matt@mudbrick.org>
2022-06-12 01:48:02 -0400

    Remove as many unwrap()s as humanly possible

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
 pub mod sporker;
+
+#[derive(Debug)]
+struct Error;
+impl std::error::Error for Error {}
+
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Went wrong didn't it")
+    }
+}

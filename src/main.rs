7a6cac375fdf6d4be467385c0d2d9c8f9d1551de
Matt Cegielka <matt@mudbrick.org>
2022-06-26 01:14:21 -0400

    Big refactor, with lots of error handling.
    We have:
    * A crate-specific Result type
    * More rigorous command parsing
    * Enum for bot commands
    * Single point of command handler dispatch
    * Better formatted code.
    * Probably still some bugs.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,10 @@ use futures::prelude::*;
 use irc::client::prelude::*;
 use tokio::sync::mpsc::unbounded_channel;
 
-use botrick::{parse_command, sporker, BotCommand, Channelizer};
+use botrick::{handle_command, parse_command, sporker, Channelizer};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let config = Config::load("config.toml")?;
-    // println!("{:?}", config);
-
     // Logger thread
     let (ltx, mut lrx): Channelizer = unbounded_channel();
     let _logger = tokio::spawn(async move {
@@ -21,7 +18,6 @@ async fn main() -> Result<()> {
             let cmd = line.command.clone();
             if let Command::PRIVMSG(_, text) = cmd {
                 if !text.starts_with('\u{001}') || text.starts_with("\u{001}ACTION") {
-                    // println!("Line: {:?}, {:?}", nick, text);
                     if let Some(n) = nick {
                         s.log_message(n, text.as_str());
                     }
@@ -30,11 +26,8 @@ async fn main() -> Result<()> {
         }
     });
 
-    // Main thread stuff resumes here
-    let db = sporker::getdb()?;
-    let s = sporker::Spork::new(db);
-
-    let mut client = Client::from_config(config).await?;
+    // Spin up IRC loop
+    let mut client = Client::from_config(Config::load("config.toml")?).await?;
     client.identify()?;
 
     let mut stream = client.stream()?;
@@ -42,90 +35,39 @@ async fn main() -> Result<()> {
 
     while let Some(message) = stream.next().await.transpose()? {
         if let Command::PRIVMSG(ref _channel, ref text) = message.command {
+            // Left for demonstrative purposes: Quick and dirty example of listening for bot's own nick
             // if text.contains(&*client.current_nickname()) {
             //     // send_privmsg comes from ClientExt
             //     // sender.send_privmsg(&channel, "beep boop").unwrap();
             // }
+
+            // Determine where to send responses, and who to ping if applicable.
             let responseplace = message.response_target().unwrap();
             let responsenick = match message.source_nickname() {
                 Some(nick) => {
-                    format!("{}:", nick)
+                    format!("{}: ", nick)
                 }
                 _ => "".to_string(),
             }
             .to_string();
 
-            // println!("source_nickname: {:?} response_target {:?} message {:?}", message.source_nickname(), message.response_target(), message);
-
+            // Attempt to parse out a valid command from the line.
+            // If it's a valid bot command, call a handler and send back the response.
+            // If it's just a regular PRIVMSG, send it to the logger task.
             let cmd = parse_command(text);
             match cmd {
-                Some(BotCommand::Bots) => {
-                    sender.send_privmsg(
-                        responseplace,
-                        "Reporting in! [Rust] just %spork or %sporklike, yo.",
-                    )?;
-                }
-                Some(BotCommand::Spork(cmd_text)) => {
-                    let words: Vec<&str> = cmd_text.split_whitespace().collect();
-                    let startw = if !words.is_empty() {
-                        println!("{} sporked {:?}", responsenick, words);
-                        s.start_with_word(words[0])
-                    } else {
-                        println!("{} sporked no words", responsenick);
-                        s.start()
-                    };
-
-                    match startw {
-                        Some(word) => {
-                            let mut words = sporker::build_words(word, &s);
-                            words.insert(0, responsenick.to_string());
-                            sender.send_privmsg(responseplace, words.join(" "))?;
+                Some(command) => {
+                    println!("{}{:?}", responsenick, command);
+                    let result = match handle_command(command) {
+                        Ok(message) => {
+                            format!("{}{}", responsenick, message)
                         }
-                        _ => {
-                            sender.send_privmsg(responseplace, "Couldn't do it could I")?;
-                        }
-                    }
-                }
-                Some(BotCommand::Sporklike(cmd_text)) => {
-                    // Get all our cmdline args
-                    let words: Vec<&str> = cmd_text.split_whitespace().collect();
-
-                    // Fewer than 2 args? Go away.
-                    if words.is_empty() {
-                        sender
-                            .send_privmsg(responseplace, "Talking about nobody is it")
-                            .unwrap();
-                        continue;
-                        // Ok(())
-                    }
-
-                    let saidby = words[0];
-
-                    // If we have more than one arg, take the first one and run with it.
-                    // Otherwise, find out own start word. With blackjack. And hookers.
-                    let startw = match words.len() {
-                        1 => {
-                            println!("{} sporkliked {}", responsenick, saidby);
-                            s.start_like(saidby)
-                        }
-                        _ => {
-                            println!("{} sporkliked {} {:?}", responsenick, saidby, words);
-                            s.start_with_word_like(words[1], saidby)
+                        Err(message) => {
+                            format!("Bot Error: {}", message)
                         }
                     };
 
-                    match startw {
-                        Some(word) => {
-                            let mut words = sporker::build_words_like(word, &s, saidby);
-                            words.insert(0, responsenick.to_string());
-                            sender.send_privmsg(responseplace, words.join(" ")).unwrap();
-                        }
-                        _ => {
-                            sender
-                                .send_privmsg(responseplace, "Couldn't do it could I")
-                                .unwrap();
-                        }
-                    }
+                    sender.send_privmsg(responseplace, result)?;
                 }
                 _ => {
                     ltx.send(message.clone())?; // Log the message if it isn't a valid command to us

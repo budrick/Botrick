5dd0c9d51d43acd2cb5e83eec72041ac39f463ac
Matt Cegielka <matt@mudbrick.org>
2022-10-22 15:16:39 -0400

    Remove debug path print

--- src/sporker/mod.rs
+++ src/sporker/mod.rs
@@ -253,7 +253,6 @@ pub fn getdb() -> Result<Connection> {
     path.push("data");
     path.push("werdz");
     path.set_extension("sqlite");
-    println!("{:?}", path);
     let h: Connection = Connection::open(path)?;
     Ok(h)
 }

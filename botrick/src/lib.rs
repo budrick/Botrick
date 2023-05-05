4d58da68ec3a3c0a519d53eced16545f962d5d69
Matt Cegielka <matt@mudbrick.org>
2023-05-05 00:36:02 -0400

    Add version to .bots

--- botrick/src/lib.rs
+++ botrick/src/lib.rs
@@ -5,3 +5,4 @@ pub mod actors;
 pub mod irc;
 
 pub const VERSION_STR: &str = concat!(env!("CARGO_PKG_NAME"), " ", env!("CARGO_PKG_VERSION"),);
+pub const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");

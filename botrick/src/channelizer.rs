c6397c24da58d19f4b2b9d67163e2330dc55d033
Budrick <budrick@mudbrick.org>
2023-05-04 23:54:44 -0400

    Remove non-actor Botrick core, leaving only nu-Botrick

--- botrick/src/channelizer.rs
+++
@@ -1,5 +0,0 @@
-use irc::proto::Message;
-use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
-
-/// Convenience type for Tokio channels passing `irc::proto::Message`
-pub type Channelizer = (UnboundedSender<Message>, UnboundedReceiver<Message>);

7c39ab5084c02fae764eda494c37b76f8d385e14
Budrick <budrick@mudbrick.org>
2023-07-30 15:24:28 -0400

    Add as-yet-unused subcommands, and better descriptions in `clap` args

--- botrick/src/args.rs
+++ botrick/src/args.rs
@@ -1,13 +1,31 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    #[arg(short, long, default_value = ".")]
+    #[command(subcommand)]
+    command: Commands,
+
+    #[arg(short, long, value_name = "DIR", default_value = ".")]
+    /// Set Botrick's root directory
     pub dir: Option<PathBuf>,
 }
 
 pub fn parse() -> Args {
     Args::parse()
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Cli {}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Create a default configuration and data files
+    Init,
+
+    /// Run the bot
+    Run,
 }

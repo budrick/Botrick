c37a8a3beb72efcb51c8592bfca907f4aca5f4c2
Matt Cegielka <matt@mudbrick.org>
2023-07-30 15:30:38 -0400

    Make subcommands optional

--- botrick/src/args.rs
+++ botrick/src/args.rs
@@ -4,12 +4,12 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    #[command(subcommand)]
-    command: Commands,
-
     #[arg(short, long, value_name = "DIR", default_value = ".")]
     /// Set Botrick's root directory
     pub dir: Option<PathBuf>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
 pub fn parse() -> Args {

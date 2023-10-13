cece46fa5b7119f81f7cac98563fa8809434e47a
Matt Cegielka <matt@mudbrick.org>
2023-10-13 17:55:34 -0400

    Include data mod

---
+++ botrick/src/data.rs
@@ -0,0 +1,14 @@
+pub const DWORD_ROOTS: &'static [&str] = &[
+    "HKLM",
+    "HKIM",
+    "HKFU",
+];
+pub const DWORDS: &'static [&str] = &[
+    "MICRODINKY",
+    "7",
+    "yes",
+    "no",
+    "null",
+    "SHUT",
+    "gregin",
+];
